f4bb3ff2e5e872435b21eeb9f74dcb4def774744
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-08-05 14:40:03 +0800

    Fixed wrong html target name in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,11 @@ module.exports = {
   },
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./src/template.html"],
+    watchFiles: ["./src/base.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: "./src/base.html",
     }),
   ],
   module: {
@@ -24,14 +24,6 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
       },
     ],
   },
