0e16371852eb85569a8ad54942fbbff7f49cdc8e
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-08-05 16:06:49 +0800

    Added module to generate basic home page

---
+++ NOTICE.md
@@ -0,0 +1,3 @@
+# Image Use
+Images used belong to their respective owners
+[Charming Outdoor Cafe Seating in Lage Vuursche](https://www.pexels.com/photo/charming-outdoor-cafe-seating-in-lage-vuursche-33239717/) by [Haberdoedas Photography](https://www.pexels.com/@haberdoedas/) on Pexels
--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-alert("Hello!");
+import "./styles.css";
+
+// Base Class for generating pages
+class Page {
+  static content = document.querySelector("#content");
+  static errorDisplay = document.createElement("div");
+
+  constructor() {
+    return;
+  }
+
+  generate = () => {
+    while (content.lastChild) {
+      content.removeChild(content.lastChild);
+    }
+
+    errorDisplay.textContent = "We apologize for the inconvenience.";
+
+    return;
+  };
+}
+
+class HomePage extends Page {
+  generate = () => {
+    const welcome = document.createElement("div");
+    const header = document.createElement("h1");
+    const image = document.createElement("img");
+    const greeter = document.createElement("div");
+    const greetings = document.createElement("h2");
+    const desc = document.createElement("p");
+
+    welcome.id = "welcome";
+    greeter.id = "greeter";
+
+    header.textContent = "Plate Leaf";
+    greetings.textContent = "Feel just like at Home";
+    desc.textContent =
+      "Everything you see, the Sun touches, and what you see will be a mirror of something nostalgic, familiar. \r\n \r\n";
+    desc.textContent +=
+      "Vivid Greens, Tender Browns, Charcoal Blacks and Clear Whites - all that is offered here is pure unbridled bliss of cuisine.";
+
+    image.setAttribute(
+      "src",
+      "https://images.pexels.com/photos/33239717/pexels-photo-33239717.jpeg",
+    );
+    image.setAttribute("width", "80%");
+
+    greeter.appendChild(greetings);
+    greeter.appendChild(desc);
+    welcome.appendChild(header);
+    welcome.appendChild(image);
+    welcome.appendChild(greeter);
+    content.appendChild(welcome);
+  };
+}
+
+let page = new HomePage();
+page.generate();
