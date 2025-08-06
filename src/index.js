8d2311901f33de949f854ca51892a5f7e78879af
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-08-06 12:33:52 +0800

    Implemented Tab Page Switching

--- src/index.js
+++ src/index.js
@@ -3,5 +3,30 @@ import "./styles.css";
 import { HomePage } from "./home.js";
 import { MenuPage } from "./menu.js";
 
-let page = new MenuPage();
+const nav = document.querySelector("nav");
+
+let page = new HomePage();
 page.generate();
+
+let currentTab = document.querySelector("#homeTab");
+
+nav.addEventListener("click", (e) => {
+  let target = e.target;
+
+  if ("active-tab" in target.classList) {
+    return;
+  }
+
+  if (target.id === "homeTab") {
+    page = new HomePage();
+  } else if (target.id === "menuTab") {
+    page = new MenuPage();
+  } else {
+    return;
+  }
+
+  currentTab.className = "";
+  currentTab = target;
+  currentTab.classList.add("active-tab");
+  page.generate();
+});
