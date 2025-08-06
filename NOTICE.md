743251f6867b08b9a2420d90b01baff4fbe7950e
AidanPaoloBelmonte <aidanpaolobelmonte@gmail.com>
2025-08-06 12:12:44 +0800

    Fully implemented Menu page

--- NOTICE.md
+++ NOTICE.md
@@ -1,3 +1,9 @@
 # Image Use
 Images used belong to their respective owners
-[Charming Outdoor Cafe Seating in Lage Vuursche](https://www.pexels.com/photo/charming-outdoor-cafe-seating-in-lage-vuursche-33239717/) by [Haberdoedas Photography](https://www.pexels.com/@haberdoedas/) on Pexels
+- [Charming Outdoor Cafe Seating in Lage Vuursche](https://www.pexels.com/photo/charming-outdoor-cafe-seating-in-lage-vuursche-33239717/) by [Haberdoedas Photography](https://www.pexels.com/@haberdoedas/) on Pexels
+- [Fried Dish with Sliced Lime](https://www.pexels.com/photo/fried-dish-with-sliced-lime-1123250/) by [Valeria Boltneva](https://www.pexels.com/@valeriya/) on Pexels
+- [Fish Salad Dish](https://www.pexels.com/photo/fish-salad-dish-262959/) by [Daniela Elena Tentis](https://www.pexels.com/@daniela-elena-tentis-118658/) on Pexels
+- [Bowl of Soup](https://www.pexels.com/photo/bowl-of-soup-3559899/) by [https://www.pexels.com/@jentstyle-photo/](jenvit keiwalinsarid) on Pexels
+- [Close-Up Photo Of Stacked Brownies](https://www.pexels.com/photo/close-up-photo-of-stacked-brownies-3026804/) by [Ella Olsson](https://www.pexels.com/@ella-olsson-572949/) on Pexels
+- []() by []() on Pexels
+- []() by []() on Pexels
--- src/menu.js
+++ src/menu.js
@@ -12,6 +12,18 @@ export class MenuPage extends Page {
     const menuMeals = document.createElement("div");
     const menuSoups = document.createElement("div");
     const menuDesserts = document.createElement("div");
+    const menuAppetizersTitle = document.createElement("h2");
+    const menuMealsTitle = document.createElement("h2");
+    const menuSoupsTitle = document.createElement("h2");
+    const menuDessertsTitle = document.createElement("h2");
+    const menuAppetizersDesc = document.createElement("p");
+    const menuMealsDesc = document.createElement("p");
+    const menuSoupsDesc = document.createElement("p");
+    const menuDessertsDesc = document.createElement("p");
+    const menuAppetizersImg = document.createElement("img");
+    const menuMealsImg = document.createElement("img");
+    const menuSoupsImg = document.createElement("img");
+    const menuDessertsImg = document.createElement("img");
 
     menu.id = "menu";
     menuContainer.id = "menu-container";
@@ -22,7 +34,51 @@ export class MenuPage extends Page {
 
     title.textContent = "Our Menu";
     desc.textContent = "A plethora of amazing foods to choose from.";
+    menuAppetizersTitle.textContent = "Appetizers";
+    menuMealsTitle.textContent = "Meals";
+    menuSoupsTitle.textContent = "Soups";
+    menuDessertsTitle.textContent = "Desserts";
+    menuAppetizersDesc.textContent =
+      "Prepare yourself with our flavorful Appetizers";
+    menuMealsDesc.textContent =
+      "Enjoy a fulfilling experience with our full Meals";
+    menuSoupsDesc.textContent = "Get a cleansing taste with our tasty Soups";
+    menuDessertsDesc.textContent =
+      "Satisfy your cravings with our sweetest Desserts";
 
+    menuAppetizersImg.setAttribute(
+      "src",
+      "https://images.pexels.com/photos/1123250/pexels-photo-1123250.jpeg",
+    );
+    menuAppetizersImg.setAttribute("width", "80%");
+    menuMealsImg.setAttribute(
+      "src",
+      "https://images.pexels.com/photos/262959/pexels-photo-262959.jpeg",
+    );
+    menuMealsImg.setAttribute("width", "80%");
+    menuSoupsImg.setAttribute(
+      "src",
+      "https://images.pexels.com/photos/3559899/pexels-photo-3559899.jpeg",
+    );
+    menuSoupsImg.setAttribute("width", "80%");
+    menuDessertsImg.setAttribute(
+      "src",
+      "https://images.pexels.com/photos/3026804/pexels-photo-3026804.jpeg",
+    );
+    menuDessertsImg.setAttribute("width", "80%");
+
+    menuAppetizers.appendChild(menuAppetizersImg);
+    menuMeals.appendChild(menuMealsImg);
+    menuSoups.appendChild(menuSoupsImg);
+    menuDesserts.appendChild(menuDessertsImg);
+    menuAppetizers.appendChild(menuAppetizersTitle);
+    menuMeals.appendChild(menuMealsTitle);
+    menuSoups.appendChild(menuSoupsTitle);
+    menuDesserts.appendChild(menuDessertsTitle);
+    menuAppetizers.appendChild(menuAppetizersDesc);
+    menuMeals.appendChild(menuMealsDesc);
+    menuSoups.appendChild(menuSoupsDesc);
+    menuDesserts.appendChild(menuDessertsDesc);
     menuContainer.appendChild(menuAppetizers);
     menuContainer.appendChild(menuMeals);
     menuContainer.appendChild(menuSoups);
